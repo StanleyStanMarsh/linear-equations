4ccd4bb9601a25630e04b34fdadeea197fcfe494
StanleyStanMarsh <iastafev56@gmail.com>
2024-11-04 01:02:42 +0300

    docs: add docs for exception

--- src/main/java/ru/spbstu/telematics/java/exceptions/WrongCoefficientsMatrixException.java
+++ src/main/java/ru/spbstu/telematics/java/exceptions/WrongCoefficientsMatrixException.java
@@ -1,5 +1,11 @@
 package ru.spbstu.telematics.java.exceptions;
 
+/**
+ * The WrongCoefficientsMatrixException class is a custom exception 
+ * that extends Java's Exception class. It is used to signal errors 
+ * related to an improperly formatted coefficients matrix, 
+ * particularly in the context of augmented matrices for linear equations.
+ */
 public class WrongCoefficientsMatrixException extends Exception
 {
     public WrongCoefficientsMatrixException(String message)
