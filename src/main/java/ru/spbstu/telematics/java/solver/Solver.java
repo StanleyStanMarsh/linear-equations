7adb956743c6a071b951abdf1078d16fb9aa7994
StanleyStanMarsh <iastafev56@gmail.com>
2024-11-04 00:57:14 +0300

    docs: add docs for Solver class

--- src/main/java/ru/spbstu/telematics/java/solver/Solver.java
+++ src/main/java/ru/spbstu/telematics/java/solver/Solver.java
@@ -6,8 +6,20 @@ import java.lang.Math;
 import ru.spbstu.telematics.java.matrix.Matrix;
 import ru.spbstu.telematics.java.exceptions.*;
 
+/**
+ * The Solver class provides static methods to manipulate matrices, specifically 
+ * for transforming a matrix into row echelon form using Gaussian method (elimination) 
+ * for solving systems of linear equations. 
+ */
 public class Solver 
 {
+    /**
+     * This utility method swaps two specified rows within the given matrix.
+     * 
+     * @param matrix The matrix to be modified.
+     * @param row1 The first row to be swapped.
+     * @param row2 The second row to be swapped.
+     */
     private static void swapRows(Matrix matrix, Integer row1, Integer row2) 
     {
         ArrayList<Double> tempRow = matrix.getRow(row1);
@@ -15,6 +27,14 @@ public class Solver
         matrix.setRow(row2, tempRow);
     }
 
+    /**
+     * Converts an augmented matrix into its row echelon form using Gaussian elimination. 
+     * This form has important applications for solving systems of linear equations.
+     * 
+     * @param augmented_matrix An augmented matrix where the last column represents the constants from the equations.
+     * @return The augmented matrix in row echelon form.
+     * @throws WrongCoefficientsMatrixException if the provided matrix does not have a valid form for transformation.
+     */
     public static Matrix toRowEchelonForm(Matrix augmented_matrix) throws WrongCoefficientsMatrixException
     {
         if (augmented_matrix.getRowCount() + 1 != augmented_matrix.getColumnCount())
@@ -63,6 +83,14 @@ public class Solver
         return c_matrix;
     }
 
+    /**
+     * Determines if a matrix is singular by checking for zero elements on the diagonal. 
+     * A singular matrix does not have a unique solution.
+     * 
+     * @param augmented_matrix The matrix to be checked for singularity.
+     * @return The index of the first row with a zero diagonal element, 
+     * indicating singularity. Returns -1 if the matrix is non-singular.
+     */
     private static Integer ifSingular(Matrix augmented_matrix)
     {
         for (int i = 0; i < augmented_matrix.getRowCount(); i++)
@@ -71,6 +99,18 @@ public class Solver
         return -1;
     }
 
+    /**
+     * Solves the system of linear equations represented by the given 
+     * augmented matrix using back substitution on its row echelon form. 
+     * Handles cases with unique, infinite, or no solutions.
+     * 
+     * @param augmented_matrix The augmented matrix representing the system of equations.
+     * @return  ArrayList<Double> containing the solutions to the system. 
+     * If the system has infinitely many solutions, the list is filled with 
+     * Double.POSITIVE_INFINITY. If inconsistent, returns an empty list.
+     * @throws WrongCoefficientsMatrixException if the provided matrix does not have 
+     * a suitable form for solving (checked during row echelon conversion).
+     */
     public static ArrayList<Double> solve(Matrix augmented_matrix) throws WrongCoefficientsMatrixException
     {
          Matrix c_matrix = Solver.toRowEchelonForm(augmented_matrix);
@@ -97,8 +137,8 @@ public class Solver
             for (int j = i + 1; j < n_col - 1; j++) 
             {
                 /* subtract all the lhs values
-                        * except the coefficient of the variable
-                        * whose value is being calculated */
+                * except the coefficient of the variable
+                * whose value is being calculated */
                 Double middle_res_i = answer.get(i);
                 Double middle_res_j = answer.get(j);
                 answer.set(
@@ -108,7 +148,7 @@ public class Solver
             }
         
             /* divide the RHS by the coefficient of the
-                    unknown being calculated */
+            unknown being calculated */
             Double dividend = answer.get(i);
             answer.set(i, dividend.doubleValue() / c_matrix.getElement(i, i).doubleValue());
         }
@@ -117,6 +157,12 @@ public class Solver
         return answer;
     }
 
+    /**
+     * Prints the solution of the system of equations. Handles messages for unique solutions, 
+     * infinitely many solutions, or no solutions.
+     * 
+     * @param answer The list of solutions obtained from solving the system.
+     */
     public static void printAnswer(ArrayList<Double> answer)
     {
         if (answer.isEmpty()) 
